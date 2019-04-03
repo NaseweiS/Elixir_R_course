4b629aba808f35482b48290030e83fc091816667
DThiel <daniel.thiel@uib.no>
2019-04-03 13:35:22 +0200

    finished 3rd script for plyr example

--- source/3rd_script.R
+++ source/3rd_script.R
@@ -14,3 +14,25 @@ gdp_per_continent <- ddply(.data = gapminder,
       .fun = function(x) mean(x$gdpPercap))
 
 write.csv(gdp_per_continent, file = "gdp_PC.csv", row.names = FALSE)
+
+
+# calculating the population on every continent and saving the outcome
+pop_per_continent <- ddply(.data = gapminder,
+                           . = "continent", 
+                           .fun = function(x) mean(x$pop))
+
+write.csv(pop_per_continent, file = "pop_PC.csv", row.names = FALSE)
+
+# to devide pop per continent in different years, create new variable
+
+pop_per_continent_and_year <- ddply(.data = gapminder,
+                                    .variables = c("year", "continent"),
+                                    .fun = function(x) mean(x$pop))
+
+# to change the order - wether first continents or first years, simply switch the two variables
+pop_per_continent_and_year <- ddply(.data = gapminder,
+                                    .variables = c("continent", "year"),
+                                    .fun = function(x) mean(x$pop))
+pop_per_continent_and_year
+
+
