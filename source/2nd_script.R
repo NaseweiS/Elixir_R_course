17a550ddf72da26d7a2cdcffb9aaec053752b401
DThiel <daniel.thiel@uib.no>
2019-04-03 13:04:25 +0200

    how to save a plot

--- source/2nd_script.R
+++ source/2nd_script.R
@@ -85,5 +85,50 @@ ggplot(data = gapminder,
   geom_smooth(method = "lm", color = "black", se = 0.9999, size = 2) +
   facet_wrap(~continent)
 
+# adding/changing labels on the plots
+ggplot(data = gapminder,
+       aes(x = gdpPercap, y = lifeExp)) + 
+  geom_point(aes(color = continent), alpha = 0.5) + scale_x_log10() + 
+  geom_smooth(method = "lm", color = "black", se = 0.9999, size = 2) +
+  facet_wrap(~continent) +
+  labs(
+    x = "GDP per Capita", 
+    y = "Life Expectancy", 
+    title = "Figure 1",
+    color = "Continent"
+    ) 
 
+# removing e.g. labels and ticks from the axis
+ggplot(data = gapminder,
+       aes(x = gdpPercap, y = lifeExp)) + 
+  geom_point(aes(color = continent), alpha = 0.5) + scale_x_log10() + 
+  geom_smooth(method = "lm", color = "black", se = 0.9999, size = 2) +
+  facet_wrap(~continent) +
+  labs(
+    x = "GDP per Capita", 
+    y = "Life Expectancy", 
+    title = "Figure 1",
+    color = "Continent"
+    ) + theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
+
+# to save the plot
+ggsave(filename = "figure1.png", width = 12, height = 10, units = "cm", dpi = 300)
+
+# the plot can be saved as a variable and then the fiel to save can be specified
+plot_to_print <- ggplot(data = gapminder,
+       aes(x = gdpPercap, y = lifeExp)) + 
+  geom_point(aes(color = continent), alpha = 0.5) + scale_x_log10() + 
+  geom_smooth(method = "lm", color = "black", se = 0.9999, size = 2) +
+  facet_wrap(~continent) +
+  labs(
+    x = "GDP per Capita", 
+    y = "Life Expectancy", 
+    title = "Figure 1",
+    color = "Continent"
+  ) + theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
+
+#if the plot isn't shown by default when asigned to a variable, the plot can be shown by calling the name of the variable
+plot_to_print
+
+ggsave(plot = plot_to_print, filename = "figure1.png", width = 12, height = 10, units = "cm", dpi = 300)
 
